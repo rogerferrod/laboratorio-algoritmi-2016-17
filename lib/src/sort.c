7a5875869b4c421a4b4918beeb684c7d711439ee
¨Roger <ferrod.roger@educ.di.unito.it>
2017-03-31 13:17:56 +0200

    update sort

--- lib/src/sort.c
+++ lib/src/sort.c
@@ -12,12 +12,12 @@
 #include <stdio.h>
 
 
-size_t insertion_sort(array_o* array, void* element) {
+size_t insertion_sort(array_o* array, ArrayCompare compare) {
 size_t i;
-ArrayCompare compare = array_get_compare(array);
+/*
  for(i = array_size(array); i > 0 && compare(array_at(array, i-1), element) > 0; --i ) {
-    array_set_elem(array, array_at(array, i-1), i); /* array->array[i] = array->array[i-1]; */
-  }
-return i;
+    array_set_elem(array, array_at(array, i-1), i); 
+  }*/
+return 0;
 }
 
--- lib/src/sort.h
+++ lib/src/sort.h
@@ -13,8 +13,8 @@
 #include "array.h"
 #include <stddef.h>
 
-typedef int (*AlgSort)(array_o*, void*);
+typedef int (*AlgSort)(array_o* array, ArrayCompare compare);
 
-extern size_t insertion_sort(array_o* array, void* element);
+extern size_t insertion_sort(array_o* array, ArrayCompare compare);
 
 /* + i vari algoritmi di ordinamento */
