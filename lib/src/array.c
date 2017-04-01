b3cb144061178bfd5a4424e07ebedfba8d529507
st159060 <ferrod.roger@educ.di.unito.it>
2017-04-01 12:57:27 -0400

    Added some documentation

--- lib/src/array.c
+++ lib/src/array.c
@@ -1,8 +1,14 @@
 /*
  *  File: array.c
- *  Author: 
+ *  Author: Riccardo Ferrero Regis, Roger Ferrod, Luca Chironna
  *
  *  Date: 
+ *
+ */
+ 
+ 
+ /*
+ * Implements an abstracted array
  *
  */
 
@@ -12,12 +18,14 @@
 #include <stdio.h>
 #include <errno.h>
 
+/* Implementation of the opaque type */
 struct _myArray {
-  void** array;
-  size_t size;
-  size_t capacity;
-  ArrayCompare compare;
+  void** array;             /* generic array */
+  size_t size;              /* size of the array */
+  size_t capacity;          /* capacity of the array */
+  ArrayCompare compare;     /* pointer to the compare function */
 };
+
 
 array_o* array_new(size_t capacity, ArrayCompare compare) {
   array_o* new_array = (array_o*) malloc(sizeof(array_o));
@@ -52,7 +60,7 @@ void* array_at(array_o* array, size_t position) {
   if(position >= array->size ) {
     fprintf(stderr, "Array index (%ld) out of bounds (0:%ld)\n", position, array->size);
     errno = ENOMEM;
-    exit(EXIT_FAILURE); /* o return NULL? */
+    exit(EXIT_FAILURE);
   }
   return array->array[position];
 }
@@ -61,30 +69,20 @@ void array_check_realloc(array_o* array){
   if(array->size >= array->capacity){
     array->capacity *= INCREMENT_FACTOR;
     array->array = realloc(array->array, array->capacity*sizeof(void*));
-  }else if (array->size <=  (array->capacity)/4){
+  } else if (array->size <=  (array->capacity)/4){
     array->capacity /= DECREMENT_FACTOR;
     array->array = realloc(array->array, array->capacity*sizeof(void*)); 
   }
 }
 
 void array_insert(array_o* array, void* element) {
-
   array_check_realloc(array);
-
-/*
-  size_t i;
-  for( i = array_size(array); i > 0 && array->compare(array->array[i-1], element) > 0; --i ) {
-    array->array[i] = array->array[i-1];
-  }
-*/
-
   array->array[array->size] = element;
   array->size ++;
   return;
 }
 
 int array_delete(array_o* array, size_t position) {
-
   if(position >= array->size ) {
     fprintf(stderr, "Array index (%ld) out of bounds (0:%ld)\n", position, array->size);
     errno = ENOMEM;
@@ -95,18 +93,20 @@ int array_delete(array_o* array, size_t position) {
   for( i = position+1; i < array_size(array); ++i) {
     array->array[i-1] = array->array[i];
   }
-  array->size-- ;
+  array->size--;
 
   array_check_realloc(array);
   return 0;
 }
 
+/*  forse non serve più */
 void array_set_elem(array_o* array, void* element, size_t position){
   if(position >= array->capacity ) {
     fprintf(stderr, "Array index (%ld) out of bounds (0:%ld)\n", position, array->size);
     errno = ENOMEM;
-    exit(EXIT_FAILURE); /* o return NULL? */
+    exit(EXIT_FAILURE);
   }
+  
   array->array[position] = element;
   return;
 }
@@ -115,8 +115,9 @@ void array_swap(array_o* array, size_t position_a, size_t position_b){
   if(position_a >= array->size || position_b>= array->size){
     fprintf(stderr, "Array index out of bounds (0:%ld)\n", array->size);
     errno = ENOMEM;
-    exit(EXIT_FAILURE); /* o return NULL? */
+    exit(EXIT_FAILURE);
   }
+  
   void* temp = array->array[position_a];
   array->array[position_a] = array->array[position_b];
   array->array[position_b] = temp;
