a1cdb6307ce79b8f30c70af45c45e720421d3009
rogyfe <rogyfe96@gmail.com>
2017-04-03 20:09:50 +0200

    esempio completo

--- esercizi/src/esercizio1.c
+++ esercizi/src/esercizio1.c
@@ -4,12 +4,14 @@
 #include <string.h>
 #include "../../lib/src/array.h"
 
-#define ARRAY_CAPACITY 20
+#define ARRAY_CAPACITY 20000000
 #define BUFFER_LENGHT  100
 
 typedef struct {
-  char *string;
-  int field1;
+  int id;
+  char *field1;
+  int field2;
+  float field3;
 } record;
 
 
@@ -19,7 +21,7 @@ static void array_print(array_o *array){
   elem = (record*)malloc(sizeof(record));
   for(i = 0; i < array_size(array); i++){
     elem = (record*)array_at(array, i);
-    printf("array[%d] = {%s,%d}\n", i, elem->string, elem->field1);
+    printf("array[%d] = {%d,%s,%d,%f}\n", i, elem->id, elem->field1, elem->field2, elem->field3);
   }
   return;
 }
@@ -39,7 +41,6 @@ int main(int argc, char* argv[]) {
     exit(EXIT_FAILURE);
   }
 
-
   array_o* array = array_new(ARRAY_CAPACITY);
   size_t buff_size = BUFFER_LENGHT;
   char *buffer;
@@ -47,20 +48,26 @@ int main(int argc, char* argv[]) {
 
   while(fgets(buffer,buff_size,file) != NULL){
     record* row = (record*)malloc(sizeof(record));
-    char *string = strtok(buffer,",");
-    char *integer = strtok(NULL,",");
+    char *raw_id = strtok(buffer,",");
+    char *raw_field1 = strtok(NULL,",");
+    char *raw_field2 = strtok(NULL,",");
+    char *raw_field3 = strtok(NULL,",");
     
-    char *field0 = malloc((strlen(string)+1)*sizeof(char));  /* +1 di \0 */  
-    strcpy(field0,string);    
-    int field1 = atoi(integer);  
+    int id = atoi(raw_id);
+    char *field1 = malloc((strlen(raw_field1)+1)*sizeof(char));  /* +1 di \0 */  
+    strcpy(field1,raw_field1);    
+    int field2 = atoi(raw_field2);
+    float field3 = atof(raw_field3);
 
-    row->string = field0;
-    row->field1 = field1; /* no malloc? implicita */
+    row->id = id; /* malloc implicita? */
+    row->field1 = field1;
+    row->field2 = field2;
+    row->field3 = field3;
     
     array_insert(array, row);
 
   }
-  /*teoricamente dovrebbe aver fatto la free di string e integer */
+  /*teoricamente dovrebbe aver fatto la free dei raw */
   
   array_print(array);
 
