a8d6c6b96592a251195274d1a301afa37acea7ba
lukakiro <l.chironna@gmail.com>
2017-04-05 21:30:45 +0200

    Aggiunto secondo parametro: serve a impostare quanti record leggere dal file

--- esercizi/src/esercizio1.c
+++ esercizi/src/esercizio1.c
@@ -12,16 +12,16 @@
 
 #define TIMER_START(timer)  (timer = clock());
 #define TIMER_STOP(timer)   (printf("Processor time used: %f s\n", \
-				    ((double) ((clock_t)clock() - timer)) / CLOCKS_PER_SEC));
-  
+            ((double) ((clock_t)clock() - timer)) / CLOCKS_PER_SEC));
+
 typedef struct {
-  int id;
-  char *field1;
-  int field2;
-  float field3;
+    int id;
+    char *field1;
+    int field2;
+    float field3;
 } record;
 
-int compare_str(char* a, char* b) {
+int compare_str(char *a, char *b) {
   return strcmp(a, b);
 /*  int r;
   r = strcmp(a, b);
@@ -30,49 +30,50 @@ int compare_str(char* a, char* b) {
   else return 0;
 */
 }
-int compare_record_field1(void* a, void* b) {
-  record* first = (record*)a;
-  record* last = (record*)b;
+
+int compare_record_field1(void *a, void *b) {
+  record *first = (record *) a;
+  record *last = (record *) b;
   return strcmp(first->field1, last->field1);
   //return compare_str(first->field1, last->field1);
 }
 
-static void array_print(array_o *array){
+static void array_print(array_o *array) {
   size_t i;
   record *elem;
-  elem = (record*)malloc(sizeof(record));
-  for(i = 0; i < array_size(array); i++){
-    elem = (record*)array_at(array, i);
-    printf("array[%d] = {%d,%s,%d,%f}\n", (int)i, elem->id, elem->field1, elem->field2, elem->field3);
+  elem = (record *) malloc(sizeof(record));
+  for (i = 0; i < array_size(array); i++) {
+    elem = (record *) array_at(array, i);
+    printf("array[%d] = {%d,%s,%d,%f}\n", (int) i, elem->id, elem->field1, elem->field2, elem->field3);
   }
   return;
 }
 
-static array_o* array_load(char *path){
-  FILE* file;
+static array_o *array_load(char *path, int record_read) {
+  FILE *file;
   file = fopen(path, "r");
   if (!file) {
     fprintf(stderr, "No such file or directory\n");
     errno = ENOENT;
     exit(EXIT_FAILURE); /* meglio NULL? */
   }
- 
-  array_o* array = array_new(ARRAY_CAPACITY);
+
+  array_o *array = array_new(ARRAY_CAPACITY);
   size_t buff_size = BUFFER_LENGHT;
   char *buffer;
-  buffer = (char*)malloc(buff_size*(sizeof(char)));
+  buffer = (char *) malloc(buff_size * (sizeof(char)));
 
   int count = 0;
-  while(fgets(buffer,buff_size,file) != NULL && count < 200000){
-    record* row = (record*)malloc(sizeof(record));
-    char *raw_id = strtok(buffer,",");
-    char *raw_field1 = strtok(NULL,",");
-    char *raw_field2 = strtok(NULL,",");
-    char *raw_field3 = strtok(NULL,",");
-    
+  while (fgets(buffer, buff_size, file) != NULL && count < record_read) {
+    record *row = (record *) malloc(sizeof(record));
+    char *raw_id = strtok(buffer, ",");
+    char *raw_field1 = strtok(NULL, ",");
+    char *raw_field2 = strtok(NULL, ",");
+    char *raw_field3 = strtok(NULL, ",");
+
     int id = atoi(raw_id);
-    char *field1 = malloc((strlen(raw_field1)+1)*sizeof(char));  /* +1 di \0 */  
-    strcpy(field1,raw_field1);    
+    char *field1 = malloc((strlen(raw_field1) + 1) * sizeof(char));  /* +1 di \0 */
+    strcpy(field1, raw_field1);
     int field2 = atoi(raw_field2);
     float field3 = atof(raw_field3);
 
@@ -80,18 +81,20 @@ static array_o* array_load(char *path){
     row->field1 = field1;
     row->field2 = field2;
     row->field3 = field3;
-    
+
     array_insert(array, row);
-    count++;
+
+    if (record_read != 0)
+      count++;
   }
   /*teoricamente dovrebbe aver fatto la free dei raw */
 
-  free(buffer);			 
+  free(buffer);
   fclose(file);
   return array;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   /*
   char* s1 = "zprova";
   char* s2 = "prova";
@@ -104,7 +107,7 @@ int main(int argc, char* argv[]) {
   fprintf(stdout, "%d\n", compare_record_field1(r1, r2));
   return 0;
   */
-  array_o* array;
+  array_o *array;
   clock_t timer;
 
   if (argc < 2) {
@@ -113,12 +116,16 @@ int main(int argc, char* argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  
+  int record_read = 1000;
+  if (argc >= 3) {
+    record_read = atoi(argv[2]);
+  }
+
   fprintf(stdout, "array_load\n");
   TIMER_START(timer);
-  array = array_load(argv[1]);
+  array = array_load(argv[1], record_read);
   TIMER_STOP(timer);
-  fprintf(stdout, "array_size: %u\n", (unsigned int)array_size(array));
+  fprintf(stdout, "array_size: %u\n", (unsigned int) array_size(array));
 /*
   sleep(1);
   
@@ -127,9 +134,9 @@ int main(int argc, char* argv[]) {
   insertion_sort(array, compare_record_field1);
   TIMER_STOP(timer);
 */
-  
+
   sleep(1);
-  
+
   fprintf(stdout, "selection_sort\n");
   TIMER_START(timer);
   selection_sort(array, compare_record_field1);
@@ -142,10 +149,10 @@ int main(int argc, char* argv[]) {
   TIMER_START(timer);
   quick_sort(array, compare_record_field1);
   TIMER_STOP(timer);
-*/  
+*/
   //sleep(2);
   //array_print(array);
-  
+
   array_free(array);
 
   return 0;
