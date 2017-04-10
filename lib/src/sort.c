cc11060c3097045407dbdf0ff47e58db5809119a
rogyfe <rogyfe96@gmail.com>
2017-04-10 17:00:07 +0200

    re-fix macro

--- lib/src/sort.c
+++ lib/src/sort.c
@@ -18,6 +18,21 @@
 #include <time.h>
 
 
+/* Pivot choice method, can be random, median, first, last */
+#define PIVOT_TYPE random
+
+#if PIVOT_TYPE == random
+  #define PIVOT_INDEX(first,last) (rand()%(last-first+1)+first)
+#elif PIVOT_TYPE == median
+  #define PIVOT_INDEX(first,last) (((last-first)/2)+first)
+#elif PIVOT_TYPE == first
+  #define PIVOT_INDEX(first,last) (first)
+#elif PIVOT_TYPE == last
+  #define PIVOT_INDEX(first,last) (last)
+#else 
+  #define PIVOT_INDEX(first,last) (rand()%(last-first+1)+first)
+#endif
+
 /* Partition needed for the quick sort */
 static void q_partition(array_o* array, int begin, int end, int *p, int *q, ArrayCompare compare);
 
