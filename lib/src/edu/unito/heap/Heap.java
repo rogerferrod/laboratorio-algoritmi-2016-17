f9f61fdb1c5570fd6566ae0053027c6c985c577f
lukakiro <l.chironna@gmail.com>
2017-05-09 13:18:28 +0200

    (es3) added isMinHeap iterativo e ricorsivo

--- lib/src/edu/unito/heap/Heap.java
+++ lib/src/edu/unito/heap/Heap.java
@@ -162,6 +162,66 @@ public class Heap<T> {
     //return (left != pos ? isMaxHeap(heap, comparator, left) : true) && (right != pos ? isMaxHeap(heap, comparator, right) : true);
   }
 
+  /**
+   * Check if heap is a maxHeap.
+   *
+   * Iterative version.
+   *
+   * @param heap Object's array representing the heap.
+   * @param comparator Method to compare two heap elements.
+   * @return true if <code>heap</code> is a maxHeap
+   */
+  public static boolean isMinHeap(Object[] heap, Comparator<Object> comparator) {
+    int size = heap.length;
+    int child;
+    for (int i=0; i<size; i++) {
+      System.out.println("-i: "+i+"  left: "+left(i, size)+"  right: "+right(i, size));
+      child = left(i, size);
+      if (child != i) {
+        if (comparator.compare(heap[i], heap[child]) > 1) {   //heap[i] >= heap[child]
+          return false;
+        }
+      }
+      child = right(i, size);
+      if (child != i) {
+        if (comparator.compare(heap[i], heap[child]) > 1) {   //heap[i] >= heap[child]
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Check if heap is a maxHeap.
+   *
+   * Recursive version.
+   *
+   * @param heap Object's array representing the heap.
+   * @param comparator Method to compare two heap elements.
+   * @return true if <code>heap</code> is a maxHeap
+   */
+  public static boolean isMinHeap(Object[] heap, Comparator<Object> comparator, int pos) {
+    int size = heap.length;
+    int left, right;
+    System.out.println("-i: "+pos+"  left: "+left(pos, size)+"  right: "+right(pos, size));
+
+    left = left(pos, size);
+    if (left != pos) {
+      if (comparator.compare(heap[pos], heap[left]) > 1) {   //heap[i] >= heap[child]
+        return false;
+      }
+    }
+    right = right(pos, size);
+    if (right != pos) {
+      if (comparator.compare(heap[pos], heap[right]) > 1) {   //heap[i] >= heap[child]
+        return false;
+      }
+    }
+    return (left == pos || isMinHeap(heap, comparator, left)) && (right == pos || isMinHeap(heap, comparator, right));
+    //return (left != pos ? isMinHeap(heap, comparator, left) : true) && (right != pos ? isMinHeap(heap, comparator, right) : true);
+  }
+
   private static int left(int pos, int size) {
     if (size > 2*pos+1)
       return 2*pos+1;
