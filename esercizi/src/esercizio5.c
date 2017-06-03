775bb2ea870ae7e1b964d9791a2e475cde234cb7
rogyfe <rogyfe96@gmail.com>
2017-06-03 02:21:11 +0200

    try to fix esercizio5

--- esercizi/src/esercizio5.c
+++ esercizi/src/esercizio5.c
@@ -29,129 +29,13 @@
 #define TIMER_START(timer)  (timer = clock());
 #define TIMER_STOP(timer)   (printf("Processor time used: %f s\n", \
             ((double) ((clock_t)clock() - timer)) / CLOCKS_PER_SEC));
-/*
-#define PRINT_RATE     0.125
 
-#define FIELD1 4
-#define FIELD2 5
-#define FIELD3 6
-
-#define ASCENDING  7
-#define DESCENDING 8
-
-#define I_SORT 1
-#define S_SORT 2
-#define Q_SORT 3
-
-*/
-/*
 typedef struct {
-    int id;
     char *field1;
-    int field2;
-    float field3;
+    char *field2;
+    double *field3;
 } record;
-*/
 
-/*
-int compare_ascending_field1(void *a, void *b) {
-  record *first = (record *) a;
-  record *last = (record *) b;
-  return strcmp(first->field1, last->field1);
-}
-
-int compare_descending_field1(void *a, void *b) {
-  record *first = (record *) a;
-  record *last = (record *) b;
-  return (-1)*strcmp(first->field1, last->field1);
-}
-*/
-
-/*
-static void array_print(array_o *array, float rate) {
-  size_t i;
-  record *elem;
-  int delta;
-
-  if (rate == 0){
-    return;
-  }
-
-  if ((int)(array_size(array) * rate) == 0) {
-    delta=1;
-  } else {
-    delta = (int) (rate * (float) array_size(array));
-  }
-
-  for (i = 0; i < array_size(array); i+=delta) {
-    elem = (record *) array_at(array, i);
-    printf("array[%d] = {%d,%s,%d,%f}\n", (int)i, elem->id, elem->field1, elem->field2, elem->field3);
-  }
-
-  i = array_size(array)-1;
-  elem = (record *) array_at(array, i);
-  printf("array[%d] = {%d,%s,%d,%f}\n", (int)i, elem->id, elem->field1, elem->field2, elem->field3);
-  return;
-}
-*/
-
-/*
-static void memory_free(array_o *array){
-  record *elem;
-  size_t i;
-
-  for (i = 0; i < array_size(array); ++i) {
-    elem = (record *) array_at(array, i);
-    free(elem->field1);
-    free(elem);
-  }
-
-  array_free(array);
-  return;
-}
-*/
-
-/*
-static record *record_load(char *buffer){
-  int id;
-  char *field1;
-  int field2;
-  float field3;
-
-  record *row = (record *) malloc(sizeof(record));
-  if (row == NULL) {
-    fprintf(stderr, "Not enough memory for new record\n");
-    errno = ENOMEM;
-    exit(EXIT_FAILURE);
-  }
-
-  char *raw_id = strtok(buffer, ",");
-  char *raw_field1 = strtok(NULL, ",");
-  char *raw_field2 = strtok(NULL, ",");
-  char *raw_field3 = strtok(NULL, ",");
-
-  id = atoi(raw_id);
-  field1 = malloc((strlen(raw_field1) + 1)*sizeof(char));
-  // +1 di \0
-
-  if (field1 == NULL) {
-    fprintf(stderr, "Not enough memory for new field1\n");
-    errno = ENOMEM;
-    exit(EXIT_FAILURE);
-  }
-
-  strcpy(field1, raw_field1);
-  field2 = atoi(raw_field2);
-  field3 = atof(raw_field3);
-
-  row->id = id;
-  row->field1 = field1;
-  row->field2 = field2;
-  row->field3 = field3;
-
-  return row;
-}
-*/
 
 static size_t djb2a(void* str) {
   return (size_t)strlen((char*)str);
@@ -164,13 +48,65 @@ static size_t djb2a(void* str) {
   }
   return hash;
 }
+
 static int compare_str(void *x, void *y){
   return strcmp(x, y);
 }
+
 static int* new_int(int value) {
   int* elem = (int*) malloc(sizeof(int));
   *elem = value;
   return elem;
+}
+
+static double* new_double(double value) {
+  double* elem = (double*) malloc(sizeof(double));
+  *elem = value;
+  return elem;
+}
+
+static void memory_free(graph_o *graph){
+  record *elem;
+  size_t i;
+  
+  /*for (i = 0; i < array_size(array); ++i) {
+    elem = (record *) array_at(array, i);
+    free(elem->field1);
+    free(elem);    
+    }*/
+  
+  graph_free(graph);
+  return;
+}
+
+static record *record_load(char *buffer){  
+  char *field1;
+  char *field2;
+  double *field3;
+
+  record *row = (record *) malloc(sizeof(record));
+  if (row == NULL) {
+    fprintf(stderr, "Not enough memory for new record\n");
+    errno = ENOMEM;
+    exit(EXIT_FAILURE);
+  }
+
+  char *raw_field1 = strtok(buffer, ",");
+  char *raw_field2 = strtok(NULL, ",");
+  char *raw_field3 = strtok(NULL, ",");
+
+  field1 = malloc((strlen(raw_field1) + 1)*sizeof(char));  /* +1 di \0 */
+  field2 = malloc((strlen(raw_field2) + 1)*sizeof(char));  /* +1 di \0 */
+  field3 = new_double(atof(raw_field3));
+
+  strcpy(field1, raw_field1);
+  strcpy(field2, raw_field2);
+
+  row->field1 = field1;
+  row->field2 = field2;
+  row->field3 = field3;
+
+  return row;
 }
 
 static graph_o *graph_load(char *path, int max_record_read) {
@@ -198,111 +134,40 @@ static graph_o *graph_load(char *path, int max_record_read) {
     exit(EXIT_FAILURE);
   }
 
-  char *field1 = malloc(buff_size*sizeof(char));
-  char *field2 = malloc(buff_size*sizeof(char));
-  char *raw_field3 = malloc(buff_size*sizeof(char));
-  double *field3 = malloc(sizeof(double));
-
   count = 0;
-  while (count < max_record_read && fgets(buffer, buff_size, file) != NULL) {
-    field1 = strtok(buffer, ",");
-    field2 = strtok(NULL, ",");
-    raw_field3 = strtok(NULL, ",");
+  //  while (count < max_record_read && fgets(buffer, buff_size, file) != NULL) {
+  while (count < 1000 && fgets(buffer, buff_size, file) != NULL) {
+    record *row;
+    row = record_load(buffer);
+    printf("[%d]  %s -> %s (%lf)\n",count, row->field1, row->field2, *row->field3);
 
-    *field3 = strtod(raw_field3, NULL);
-    //*field3 = atof(raw_field3);
-
-    printf("_field1: %s\n", field1);
-    printf("_field2: %s\n", field2);
-    printf("_field3: %lf (%s)\n\n", *field3, raw_field3);
-
-    if (!graph_contains_vertex(graph, field1)) {
-      graph_add(graph, field1);
+    if(!graph_contains_vertex(graph, row->field1)){
+      graph_add(graph, row->field1);
     }
-    if (!graph_contains_vertex(graph, field2)) {
-      graph_add(graph, field2);
+    if(!graph_contains_vertex(graph, row->field2)){
+      graph_add(graph, row->field2);
     }
-    if (graph_contains_edge(graph, field1, field2)) {
-      fprintf(stderr, "EDGE (%s - %s) ESISTE GIà\n", field1, field2);
-      exit(EXIT_FAILURE);
-    }
-    graph_connect(graph, field1, field2, field3, DIRECTED);
-
-/*
-    if (count % 50 == 0) {
-      printf("%d - %s, %s, %s(%.12lf)\n", count, field1, field2, raw_field3, *field3);
-    }
-*/
+    //graph_connect(graph, row->field1, row->field2, row->field3, NO_DIRECTED);
+    graph_connect(graph, row->field1, row->field2, new_double(0.5), NO_DIRECTED);
+    
     count++;
   }
 
+  
   free(buffer);
   fclose(file);
   return graph;
 }
 
-/*
-static void array_sort(array_o *array, int algorithm, int field, int order){
-  ArrayCompare compare_pnt;
-  AlgSort sort_pnt;
-  clock_t timer;
-
-  switch(field){
-    case FIELD1:
-      fprintf(stdout, "field1 - ");
-      compare_pnt = (order == ASCENDING)? compare_ascending_field1 : compare_descending_field1;
-      break;
-    case FIELD2:
-      fprintf(stdout, "field2 - ");
-      compare_pnt = (order == ASCENDING)? compare_ascending_field2 : compare_descending_field2;
-      break;
-    case FIELD3:
-      fprintf(stdout, "field3 - ");
-      compare_pnt = (order == ASCENDING)? compare_ascending_field3 : compare_descending_field3;
-      break;
-  }
-
-  switch(algorithm){
-    case I_SORT:
-      fprintf(stdout, " insertion_sort\n");
-      sort_pnt = insertion_sort;
-      break;
-    case S_SORT:
-      fprintf(stdout, " selection_sort\n");
-      sort_pnt = selection_sort;
-      break;
-    case Q_SORT:
-      fprintf(stdout, " quicksort\n");
-      sort_pnt = quick_sort;
-      break;
-  }
-
-  // Sorting
-
-  TIMER_START(timer);
-  sort_pnt(array, compare_pnt);
-  TIMER_STOP(timer);
-
-  sleep(1);
-
-  return;
-}
-*/
 
 int main(int argc, char *argv[]) {
   graph_o *graph;
   int max_record_read;
-//  int algorithm;
-//  int field;
-//  int order;
   clock_t timer;
 
 
   /* Default settings */
   max_record_read = -1;
-//  algorithm = Q_SORT;
-//  field = FIELD1;
-//  order = ASCENDING;
 
   /* Parsing arguments */
   if (argc < 2) {
@@ -314,26 +179,6 @@ int main(int argc, char *argv[]) {
   if (argc >= 3){
     max_record_read = atoi(argv[2]);
   }
-/*
-  if (argc >= 4) {
-    if(strcmp(argv[3], "isort") == 0){algorithm = I_SORT;}
-    else if(strcmp(argv[3], "ssort") == 0){algorithm = S_SORT;}
-    else if(strcmp(argv[3], "qsort") == 0){algorithm = Q_SORT;}
-  }
-*/
-/*
-  if (argc >= 5) {
-    if(strcmp(argv[4], "field1") == 0){field = FIELD1;}
-    else if(strcmp(argv[4], "field2") == 0){field = FIELD2;}
-    else if(strcmp(argv[4], "field3") == 0){field = FIELD3;}
-  }
-*/
-/*
-  if (argc >= 6) {
-    if(strcmp(argv[5], "ascending") == 0){order = ASCENDING;}
-    else if(strcmp(argv[5], "descending") == 0){order = DESCENDING;}
-  }
-*/
 
   /* Load array */
   fprintf(stdout, "graph_load\n");
@@ -342,48 +187,13 @@ int main(int argc, char *argv[]) {
   TIMER_STOP(timer);
   fprintf(stdout, "graph_loaded\n");
 
-  graphIterator *vIter = graph_vertex_iter_init(graph);
-  int i=0;
-  void *v1, *adj;
-  while(graph_vertex_iter_hasNext(graph, vIter)) {
-    graph_vertex_iter_next(graph, vIter, &v1, &adj);
-    printf("%s (%d)\n", (char*)v1, i);
-    ++i;
-    if (i>10) break;
-/*
-    graphIterator *eIter = graph_edge_iter_init(graph, v1);
-    void *v2;
-    double *w;
-    while(graph_edge_iter_hasNext(graph, v1, eIter)) {
-      graph_edge_iter_next(graph, v1, eIter, &v2, &w);
-      printf(" - %s - %lf\n", (char*)v2, *w);
-    }
-    free(eIter);
-*/
-  }
-  free(vIter);
-
-  TIMER_START(timer);
-  fprintf(stdout, "graph_order: %u\n", (unsigned int) graph_order(graph));
-  //fprintf(stdout, "graph_size: %u\n", (unsigned int) graph_size(graph));
-  TIMER_STOP(timer);
-
-/*
-  sleep(1);
-*/
-/*
-  // Kruskal
-  //kruskal(graph);
-*/
-/*
-  //Print array
-  //array_print(array, PRINT_RATE);
-*/
+  printf("graph != NULL? %d\n", graph != NULL);
+  printf("order %d\n", graph_order(graph));
+  printf("size %d\n", graph_size(graph));
+  printf("weight %f\n", (float)graph_weight(graph));
 
   /* Free graph */
-  TIMER_START(timer);
-  graph_free(graph);
-  TIMER_STOP(timer);
+  memory_free(graph);
 
   return 0;
 }
