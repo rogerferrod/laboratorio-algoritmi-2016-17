01ef58136cd3b9f65a4e09f6caedb1eef6d1d0f0
st159104-Riccardo <ferrero_regis.riccardo@educ.di.unito.it>
2017-06-13 23:33:13 +0200

    Update lib.h

--- lib/src/lib.h
+++ lib/src/lib.h
@@ -19,9 +19,10 @@
 #define TIMER_START(timer)  (timer = clock());
 #define TIMER_STOP(timer, msg)  (printf("Processor time used: %f s (%s)\n", ((double)((clock_t)clock() - (timer))) / (CLOCKS_PER_SEC), (msg)));
 
-
+/* Success or die malloc */
 extern void* xmalloc(size_t);
 
+/* Success or die realloc */
 extern void* xrealloc(void *pointer, size_t size);
 
 
